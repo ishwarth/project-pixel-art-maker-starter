db5f5c743c77880d9f6aa8a0eb455f73f75311bf
Ishwaragouda T Hosagoudar <ishwarth@in.ibm.com>
2018-04-04 14:51:00 +0530

    committing my project pixel art maker

---
+++ designs.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('#sizePicker').submit(function makeGrid(grid) {
+        $('table tr').remove();
+        var rows = $('#inputHeight').val();
+        var cols = $('#inputWidth').val();
+        for (var i = 1; i <= rows; i++) {
+            $('table').append('<tr></tr>');
+            for (var j = 1; j <= cols; j++) {
+                $('tr:last').append('<td></td>');
+                $('td').attr("class", 'cells');
+            }
+        }
+        grid.preventDefault();
+        $('.cells').click(function (event) {
+            var paint = $('#colorPicker').val();
+            $(event.target).css('background-color', paint);
+        });
+    });
+});
